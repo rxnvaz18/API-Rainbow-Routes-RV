1be16d50dac569069858fd172ef650dea60d84e4
unknown <roxanvaz31@gmail.com>
2024-04-06 13:10:39 -0700

    finished assignment

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 require('dotenv').config()
 
 // Require needed node modules
-const express = require('express')
+import express from 'express'
 
 // Initialize your application by calling the function returned by the express module
 const app = express()
@@ -10,7 +10,7 @@ const app = express()
 // Declare routes that people can visit on the application
 
 // White Page
-app.get('/', function (req, res) {
+app.get('/', function (req: express.Request, res: express.Response):void {
     res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: white;">
@@ -24,7 +24,8 @@ app.get('/', function (req, res) {
 
 // Color Page
 app.get('/:color', function (req, res) {
-    let myColor = req.params.color
+    let myColor: unknown = req.params.color
+    if (typeof(myColor) === 'string') {
     res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
@@ -33,8 +34,8 @@ app.get('/:color', function (req, res) {
             <h1 style="color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)} Page</h1>
         </body>
     `)
+    }
 })
-
 
 // Listen to a port number defined by a local environment variable
 app.listen(process.env.PORT)
